84a218f2a82d2eb65c8fef9546130d35a0bb7c93
Mudmi <mmmudmi@gmail.com>
2023-12-18 16:48:02 +0700

    feat: show drawn result and able to copy to dashboard

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="page">
-    <Navbar :dropDownID="dropDownID" />
+    <Navbar v-model="dropDownID" />
     <div class="content">
       <div class="info">
-        <button class="yellow-btn">สุ่มร้านค้า</button>
+        <v-alert v-if="showCopySuccess" text="Copied to clipboard!" type="success" style="position: fixed; top: 1pc; z-index: 10;"></v-alert>
+        <button v-if="isDrawing" class="yellow-btn" @click="random()">สุ่มร้านค้า</button>
+        <div v-else class="show-result" >
+          <button class="info-frame" @click="copyToClipboard(this.name)">{{ this.name }}</button>
+          <button class="info-frame" @click="copyToClipboard(this.description)">{{ this.description }}</button>
+        </div>
         <DeleteAll />
       </div>
     </div>
@@ -13,21 +18,68 @@
 <script>
   import Navbar from '@/components/Navbar.vue'
   import DeleteAll from '@/components/DeleteAll.vue'
-
   import axios from 'axios';
-
   export default {
     name: "Main",
-    computed: {
-    },
     components: {Navbar,DeleteAll},
     data(){
       return{
-        dropDownID: 0,
+        dropDownID: null,
+        isDrawing: true,
+        showCopySuccess: false,
+        name: "",
+        description: "",
       }
+
     },
     methods: {
+      random(){
+        this.dropDownID = localStorage.getItem('dropDownID');
+        if (this.dropDownID == null) {
+          alert("เลือกหมวดร้านค้าก่อนกดสุ่ม")
+        } else {
+          console.log(this.dropDownID)
+          axios.get('http://localhost:80/api/shops/draw/tag/'+this.dropDownID)
+            .then((res) => {
+              this.name = res.data.name;
+              this.description = res.data.description;
+              this.isDrawing = false;
+            })
 
+        }
+      },
+      copyToClipboard(text){
+        const textArea = document.createElement('textarea');
+        textArea.value = text;
+        // Make the textarea out of viewport
+        textArea.style.position = 'fixed';
+        textArea.style.top = '0';
+        textArea.style.left = '0';
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        textArea.style.padding = '0';
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        textArea.style.background = 'transparent';
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+          const successful = document.execCommand('copy');
+          const message = successful ? 'Copied to clipboard!' : 'Failed to copy to clipboard';
+          this.showCopySuccess = true;
+          setTimeout(() => {
+            this.showCopySuccess = false;
+          }, 600);
+        } catch (err) {
+          console.error('Unable to copy to clipboard', err);
+        }
+
+        document.body.removeChild(textArea);
+      },
     }
 
   }
@@ -55,6 +107,24 @@
   align-items: center;
   justify-content: center;
 }
+
+.info-frame{
+  border-radius: 40px;
+  color: #ffffff;
+  border: 1.6px solid #ffffff ;
+  padding: 11px 10px 11px 10px;
+  font-size: 17px;
+  margin: 0.5pc;
+  transition:  0.3s ease;
+  width: 39pc;
+  height: 3.5pc;
+  overflow: auto
+}
+.info-frame:hover{
+  background-color: #ffffff;
+  color: rgb(0, 0, 0);
+}
+
 .yellow-btn{
   border-radius: 40px;
   background-color: #FF922E;
@@ -72,5 +142,11 @@
   font-size: 32px;
   width: 16pc;
 }
-
+.show-result{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center; /* Center text horizontally */
+  height: 100%; }
 </style>
